6e14dde00fdee02a2876f3f22b51175c6969feed
botsonn <ahmdghyly09@gmail.com>
2024-06-07 23:00:50 +0300

    Update ترحيب المطور.py

--- ZeMusic/plugins/play/ترحيب المطور.py
+++ ZeMusic/plugins/play/ترحيب المطور.py
@@ -6,18 +6,18 @@ import os
 
 @app.on_chat_member_updated(filters=lambda _, response: response.new_chat_member, group=847)
 async def WelcomeDev(_, response: ChatMemberUpdated):
-    dev_id = 5160061304 # حط ايديك هنا
-    if response.from_user.id == dev_id:
-        info = await app.get_chat(dev_id)
-        name = info.first_name
-        bio = info.bio
-        markup = InlineKeyboardMarkup([
-            [InlineKeyboardButton(name, user_id=dev_id)]
-        ])
-        await app.download_media(info.photo.big_file_id, file_name=os.path.join("downloads", "developer.jpg"))
-        await app.send_photo(
-            chat_id=response.chat.id,
-            reply_markup=markup,
-            photo="downloads/developer.jpg", 
-            caption=f"↢ لقد انضم مطور السورس هنا 🫦<a href='tg://user?id={dev_id}'> {name}</a> \n يرجي من الاعضاء احترام وجوده 🍻"
-)
+    dev_id = 5160061304 # حط ايديك هنا
+    if response.from_user.id == dev_id:
+        info = await app.get_chat(dev_id)
+        name = info.first_name
+        bio = info.bio
+        markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton(name, user_id=dev_id)]
+        ])
+        await app.download_media(info.photo.big_file_id, file_name=os.path.join("downloads", "developer.jpg"))
+        await app.send_photo(
+            chat_id=response.chat.id,
+            reply_markup=markup,
+            photo="downloads/developer.jpg", 
+            caption=f"↢ لقد انضم مطور السورس هنا ♥️<a href='tg://user?id={user_id}'> {name}</a> \n يرجي من الاعضاء احترام وجوده ☕🍀"
+        )
